c2d3c3e7c34641c8f7c9f20a59c79c54765a91e7
Kilian De Bock <Stualyttle@users.noreply.github.com>
2021-08-16 17:17:27 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import time
+import logging
+
+start_time = time.time()
+seconds = 5
+
+while True:
+    current_time = time.time()
+    elapsed_time = current_time - start_time
+
+    if elapsed_time > seconds:
+        # Update computer
+        print("Updating...")
+        logging.info("Updating...")
+        os.system("sudo apt update")
+        print("Updated, now upgrading...")
+        logging.info("Updated, now upgrading...")
+        os.system("sudo apt upgrade")
+        print("Upgraded.")
+        logging.info("Upgraded.")
+
+        # Reset counter and set next update to every 12 hours:
+        elapsed_time = 0
+        start_time = time.time()
+        seconds = 43200
+
